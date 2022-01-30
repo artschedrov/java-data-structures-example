c199e04daf60f39dc1f5b88bae832007436ff406
Art Schedrov <a.sch@tutanota.com>
2022-01-31 06:29:41 +0700

    arrays

---
+++ src/main/java/DataStructures/TheArrays.java
@@ -0,0 +1,28 @@
+package DataStructures;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+
+        int[] numbers = {100, 200};
+        int[] numbers2 = new int[2];
+
+        System.out.println(Arrays.toString(numbers));
+
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+//        for (String color : colors) {
+//            System.out.println(color);
+//        }
+
+        //Arrays.stream(colors).forEach(System.out::println);
+    }
+}
