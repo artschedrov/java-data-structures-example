432d41bd7ed4e142d821b5c9118f5b2ce07abb57
Art Schedrov <a.sch@tutanota.com>
2022-02-06 12:57:12 +0700

    add Set

---
+++ src/main/java/DataStructures/TheSet.java
@@ -0,0 +1,44 @@
+package DataStructures;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class TheSet {
+    public static void main(String[] args) {
+        Set<Ball> balls = new HashSet<>();
+        balls.add(new Ball("black"));
+        balls.add(new Ball("red"));
+
+        System.out.println(balls.size());
+        balls.forEach(System.out::println);
+    }
+
+    //record Ball(String color) {}
+
+    static class Ball {
+        String color;
+
+        public Ball(String color) {
+            this.color = color;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ball ball = (Ball) o;
+            return Objects.equals(color, ball.color);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(color);
+        }
+
+        @Override
+        public String toString() {
+            return "Ball{" + "color='" + color + '\'' + '}';
+        }
+    }
+}
